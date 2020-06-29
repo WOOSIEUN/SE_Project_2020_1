ff82bc532b2dab30c2fd891de7e2f82907ee4a93
ljh1240 <37605643+ljh1240@users.noreply.github.com>
2020-06-29 21:28:15 +0900

    Latest ver of Register

--- MoyeoraScheduler/src/Register.java
+++ MoyeoraScheduler/src/Register.java
@@ -14,27 +14,33 @@ public class Register extends JFrame{
 		JLabel l1 = new JLabel("ID");
 		JLabel l2 = new JLabel("Password");
 		JLabel l3 = new JLabel("Password Check");
+		JLabel l4 = new JLabel("Name");
 		JButton b1 = new JButton("Register");
 		JButton b2 = new JButton("Cancel");
 		add(l1);
 		add(l2);
 		add(l3);
+		add(l4);
 		add(b1);
 		add(b2);
 		JTextField t1 = new JTextField();
 		JTextField t2 = new JTextField();
 		JTextField t3 = new JTextField();
+		JTextField t4 = new JTextField();
 		add(t1);
 		add(t2);
 		add(t3);
+		add(t4);
 		l1.setBounds(250,150,100,15); //ID
 		l2.setBounds(250,175,100,15); //Password
 		l3.setBounds(250,200,100,15); //Password Check
+		l4.setBounds(250,225,100,15); //Name
 		b1.setBounds(275,250,85,15); //Enroll
 		b2.setBounds(375,250,85,15); //Cancel
 		t1.setBounds(350,150,100,15);
 		t2.setBounds(350,175,100,15);
 		t3.setBounds(350,200,100,15);
+		t4.setBounds(350,225,100,15);
 		DB a = new DB();
 		
 		setSize(750,500);
@@ -48,13 +54,14 @@ public class Register extends JFrame{
 				try {
 					String s1; //ID
 					s1=t1.getText();
-					String s2, s3; //Password & Password Check
+					String s2, s3, n; //Password, Password Check & Name
 					s2=t2.getText();
 					s3=t3.getText();
+					n=t4.getText();
 					BufferedWriter bos = new BufferedWriter(new FileWriter("ID_List.txt",true));
 					if(s3.equals(s2)) { //비밀번호가 제대로 확인됐을 시
 						System.out.println("Id: "+ s1 + " " + "Pw: "+s2);
-						//a.Rinsert(s1,s2); //DB에 ID와 Password 삽입
+						a.Rinsert(s1,s2,n); //DB에 ID와 Password 삽입
 						JOptionPane.showMessageDialog(null, "Success Register");
 						dispose(); // Main 화면으로 이동 시 dispose() 삭제 후 새로운 클래스 생성으로 대체
 					}
