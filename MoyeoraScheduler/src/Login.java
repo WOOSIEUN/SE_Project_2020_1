bb119f245131e8ebee5d160fbb9905095170f002
ljh1240 <37605643+ljh1240@users.noreply.github.com>
2020-06-30 21:08:51 +0900

    updated ver

--- MoyeoraScheduler/src/Login.java
+++ MoyeoraScheduler/src/Login.java
@@ -10,12 +10,14 @@ import java.util.Arrays;
 public class Login extends JFrame{
 
 	public String Id;
+	public String Name;
+	boolean ism = false;
 	public Login(){
 		DB a = new DB();
-		boolean ism = false;
 		JPanel p = new JPanel();
 		JButton j1 = new JButton("Register");
 		JButton j2 = new JButton("Login");
+		JButton j3 = new JButton("Admin");
 		JTextField t1 = new JTextField();
 		JPasswordField t2 = new JPasswordField();
 		JTextField t3 = new JTextField();
@@ -26,6 +28,7 @@ public class Login extends JFrame{
 		l0.setBounds(100,50,100,100);//제목
 		j1.setBounds(360,250,90,20); //회원가입
 		j2.setBounds(360,225,90,20); //로그인
+		j3.setBounds(360,275,90,20); //관리자 전용 로그인
 		t1.setBounds(350,150,100,15); //아이디칸
 		t2.setBounds(350,175,100,15); //비밀번호칸
 		t3.setBounds(350,200,100,15); //이름칸
@@ -34,6 +37,7 @@ public class Login extends JFrame{
 		l3.setBounds(280,200,75,15); //이름
 		add(j1);
 		add(j2);
+		add(j3);
 		add(t1);
 		add(t2);
 		add(t3);
@@ -64,9 +68,11 @@ public class Login extends JFrame{
 					pw += Character.toString(cha);
 				}
 
-				if(a.find(s1,pw)) // DB에 담긴 ID와 Password와 내가 입력한 ID와 Password가 일치하는지 확인해주는 메소드
+				if(a.find(s1,pw,s3)) // DB에 담긴 ID와 Password와 내가 입력한 ID와 Password, 이름이 일치하는지 확인해주는 메소드
 				{
 					Id = s1;
+					Name = s3;
+					ism = false;
 					JOptionPane.showMessageDialog(null, "Success Login!");
 					MainFrame gui = new MainFrame(s1, s3, ism); //메인 화면으로 이동
 					System.out.println("Id in Login: "+s1);
@@ -76,8 +82,47 @@ public class Login extends JFrame{
 
 				else
 				{
-					JOptionPane.showMessageDialog(null, "Wrong ID or Password");
+					JOptionPane.showMessageDialog(null, "Wrong ID or Password or Name");
 				}		
+			}
+		});
+		
+		j3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e3) {
+				String s1, s3; 
+				char[] s2 = new char[20];
+				String ad = "";
+				s1=(String) t1.getText();
+				s2=t2.getPassword();
+				s3=t3.getText();
+				
+				for(char cha : s2) {
+					Character.toString(cha);
+					ad += (ad.equals(""))?""+cha+"":""+cha+"";
+				}
+
+				if(a.find(s1,ad,s3))
+				{
+					if(a.ad_find(s1))
+					{
+						Id = s1;
+						Name = s3;
+						ism = true;
+						JOptionPane.showMessageDialog(null, "Hello, Sir!");
+						MainFrame gui = new MainFrame(s1, s3, ism); //메인 화면으로 이동
+						System.out.println("Id in Login: "+s1);
+						gui.setVisible(true);
+						dispose();
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null, "This Id is not Admin!");
+					}
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Wrong ID or Password or Name");
+				}
 			}
 		});
 	}
@@ -85,11 +130,6 @@ public class Login extends JFrame{
 	public String getId()
 	{
 		return Id;
-	}
-	
-	public String get_Name()
-	{
-		return Name;
 	}
 
 	public static void main(String[] args)
